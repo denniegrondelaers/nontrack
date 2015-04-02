4f3209ced6ecf04049a00cb904dcfb3f81e2ccd1
Dennie Grondelaers <dennie.grondelaers@gmail.com>
2015-04-02 13:21:41 +0200

    First commit

---
+++ app/src/main/java/be/freyja/nontrack/PastTracksFragment.java
@@ -0,0 +1,24 @@
+package be.freyja.nontrack;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PastTracksFragment extends Fragment {
+
+    public PastTracksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_past_tracks, container, false);
+    }
+
+
+}
